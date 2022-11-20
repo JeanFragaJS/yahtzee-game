bf45672edf5b1894f2c3c72bfe0344cd90faed4c
Jean Fraga <jeandevjs@gmail.com>
2022-11-20 12:26:38 -0300

    feat: add freaquency method

--- src/rules/rules.js
+++ src/rules/rules.js
@@ -6,7 +6,26 @@ class Rules {
   }
 
   sum ( dices ) {
-    //soma todos os dados
+    /* 
+      * soma todos os dados
+      * dice =  [2,4,6,1,3]
+      * output 16;
+    */  
     return dices.reduce((prev, curr) => prev + curr); 
   }
+
+  frequency ( dices) {
+    /* 
+      * Frequencia que cada valor aparece nos 5 dados
+      * dice =  [2,6,6,1,3]
+      * Map = [[2,1], [6,2]...]
+      * output = [1,2,...]
+    */ 
+    let freqs = new Map();
+    for (let val of dices) freqs.set(val, (freqs.get(val) || 0) + 1);
+    return Array.from(freqs.values()); 
+  }
+
+
+
 }
