81c37c7ac5cf4762915321d3b4e6e5aa0539537b
Jean Fraga <jeandevjs@gmail.com>
2022-11-21 11:58:06 -0300

    add dices icons

--- src/die/Die.js
+++ src/die/Die.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix} from '@fortawesome/free-solid-svg-icons';
 
+import "./Die.css"; 
 
 
 class Die extends Component {
   static defaultProps =  {
-    numberWords: ["one", "two", "three", "four", "five", "six"]
+    dicesList: [faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix]
   }
   constructor (props) {
     super(props);
@@ -17,11 +19,12 @@ class Die extends Component {
   }
   render () {
     const {disabled, locked, isRolling, val} = this.props;
-    let classes =  `Die-${this.props.numberWords[this.props.idx]}`;
+    let classes =  `Die`;
+    let dice = this.props.dicesList[val-1];
     if ( locked ) classes += " Die-locked";
     if ( isRolling ) classes += " Die-isRolling";
     return (
-      <FontAwesomeIcon className={classes} onClick={this.handleClick} icon={this.props.val} disabled={disabled}/>
+      <FontAwesomeIcon className={classes} onClick={this.handleClick} icon={dice} disabled={disabled} size="5x"/>
     )
   }
 }
