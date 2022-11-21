9adcf985e4f90101303ca2467f96e96a3ef7aff5
Jean Fraga <jeandevjs@gmail.com>
2022-11-21 11:58:52 -0300

    add isRolling prop

--- src/dice/dice.js
+++ src/dice/dice.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Die from '../die/Die';
+import "./Dice.css";
 
 class Dice extends Component {
   constructor (props) {
@@ -8,17 +9,17 @@ class Dice extends Component {
 
   render () {
     return (
-    <div>
+    <div className='Dice'>
       {   
         this.props.dices.map((val, idx) => (
         <Die 
-          icon={val} 
+          val={val} 
           key={idx}
           idx={idx}
           dieHandleClick={this.props.diceHandleClick}
           locked={this.props.locked[idx]}
           disabled={this.props.disabled}
-
+          isRolling={this.props.isRolling && !this.props.locked[idx]}
         /> 
         ))
       }
